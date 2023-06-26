3964e288ba2d7e73cee74495ec7ee0ff059c9702
geh657 <2200515@ecc.ac.jp>
2023-06-26 16:42:29 +0900

    feat : Firebaseの連携

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HomeScreen from './components/screens/HomeScreen'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import NotFoundScreen from './components/screens/NotFoundScreen'
-import GameScreen from './components/screens/GameScreen'
-import HistoryScreen from './components/screens/HistoryScreen'
-import RankingScreen from './components/screens/RankingScreen'
-import AboutScreen from './components/screens/AboutScreen'
-import SettingsScreen from './components/screens/SettingsScreen'
 
 import "./styles/index.css"
 
@@ -26,16 +19,7 @@ root.render(
 				<div className="width-800 min-height-500 max-height-screen-90 overflow-y-auto border border-neutral-700 border-4 rounded-xl bg-window">
 
 					<Routes>
-
-						<Route path="/" element={<HomeScreen />} />
-						<Route path="/game" element={<GameScreen />} />
-
-						<Route path="/history" element={<HistoryScreen />} />
-						<Route path="/settings" element={<SettingsScreen />} />
-						<Route path="/ranking" element={<RankingScreen />} />
-						<Route path="/about" element={<AboutScreen />} />
-
-						<Route path="*" element={<NotFoundScreen />} />
+						<Route path="/" element={<div>Home</div>} />
 					</Routes>
 				</div>
 			</div>
